93889c30282cb8f89e32125384d5b2927c8412c2
lowks <kianseong@gmail.com>
2015-06-17 23:52:58 +0800

    Fix test using elixir: 1.0.4

--- test/opbeat/error_formatter_test.exs
+++ test/opbeat/error_formatter_test.exs
@@ -22,7 +22,7 @@ defmodule Opbeat.ErrorFormatterTest do
             %{filename: "test/opbeat/error_formatter_test.exs", function: "test format_stacktrace - returns correct values for error", lineno: 18},
             %{filename: "lib/ex_unit/runner.ex", function: "exec_test", lineno: 249},
             %{filename: "timer.erl", function: "tc", lineno: 165},
-            %{filename: "lib/ex_unit/runner.ex", function: "-spawn_test/3-fun-1-", lineno: 199}]
+            %{filename: "lib/ex_unit/runner.ex", function: "-spawn_test/3-fun-1-", lineno: 201}]
           assert Opbeat.ErrorFormatter.format_stacktrace(exception) == %{frames: expected}
       end
     end
